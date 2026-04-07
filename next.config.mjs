3c8107c909dd5149108e2d56eb02f90e1f4703a9
John Becerra <cardonabecerrajonathanmanuel@gmail.com>
2026-04-07 15:07:06 -0600

    Cambio archivo next

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,21 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        // 1. Rewrite para las páginas visibles
+        // 1. Mapea las páginas visibles
         source: '/landings/:path*',
         destination: 'https://cefin-landings-z9uk.vercel.app/landings/:path*', 
       },
       {
-        // 2. NUEVO: Rewrite para los archivos internos de Next.js (JS, CSS, Chunks)
-        // Esto evita los errores 404 que ves en la consola
+        // 2. Mapea los archivos estáticos (CSS y JS)
+        // Esto quita los errores 404 de los chunks
         source: '/_next/static/:path*',
         destination: 'https://cefin-landings-z9uk.vercel.app/_next/static/:path*',
+      },
+      {
+        // 3. Mapea el optimizador de imágenes de Next.js
+        // Esto quita el error 400 de alfredo.png
+        source: '/_next/image/:path*',
+        destination: 'https://cefin-landings-z9uk.vercel.app/_next/image/:path*',
       },
     ]
   },
