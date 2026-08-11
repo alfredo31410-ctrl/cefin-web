8ca11b6e9a8aad34d0d45d761e2d9fa4b91aac82
JohnBecerra16 <cardonabecerrajonathanmanuel@gmail.com>
2026-08-11 14:36:42 -0600

    Actualizar imagen del curso de restaurantes

--- data/cursos.ts
+++ data/cursos.ts
@@ -309,7 +309,7 @@ La formación incluye 6 meses de sesiones en vivo del Factor CEFIN todos los lun
   descripcionLarga:
     "Especialízate como asesor fiscal para restaurantes y negocios de comida y fortalece tu asesoría para este sector.",
   precio: "$3,687 MXN",
-  imagen: "/cefin-contable.jpg",
+  imagen: "/BANNER - RESTAURANTES para web.png",
   categoria: "contabilidad",
   instructor: "CEFIN - Contabilidad e Impuestos",
   destacado: true,
